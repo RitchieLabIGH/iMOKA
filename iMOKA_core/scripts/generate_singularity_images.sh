a12cdc5afd11e4a78ed1060689247171d34402f7
Claudio Lorenzi <claudio.lorenzi@igh.cnrs.fr>
2020-04-15 13:24:54 +0200

    Bugfix singulatiry generation

--- iMOKA_core/scripts/generate_singularity_images.sh
+++ iMOKA_core/scripts/generate_singularity_images.sh
@@ -11,14 +11,11 @@ cd ${basefolder}
 
 
 sudo singularity build -F ${singularity_final_name} docker://cloxd/imoka:1.0
-rm ${singularity_final_recipe}.tmp
 
 
-if [[ "${extended_build}" == "T" ]]; then
-	awk -v base_img="${singularity_final_name}" '{if ($1 == "From:" ) {line=$1 " " base_img } else {line=$0 } ; print line}'  ${singularity_extended_recipe} > ${singularity_extended_recipe}.tmp
-	sudo singularity build -F ${singularity_extended_name} ${singularity_extended_recipe}.tmp
-	rm ${singularity_extended_recipe}.tmp
-fi
+awk -v base_img="${singularity_final_name}" '{if ($1 == "From:" ) {line=$1 " " base_img } else {line=$0 } ; print line}'  ${singularity_extended_recipe} > ${singularity_extended_recipe}.tmp
+sudo singularity build -F ${singularity_extended_name} ${singularity_extended_recipe}.tmp
+rm ${singularity_extended_recipe}.tmp
 
 
 
